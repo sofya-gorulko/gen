1077237c2b3418d1faf8b8a8154c1fbab76f66f7
sg <sofya.gorulko@yandex.ru>
2026-03-13 10:54:36 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+struct gen_reading {
+    std::string gen;
+    std::string quality;
+    int length;
+    gen_reading(const std::string& head1, std::string gen, std::string quality) {
+        this->gen = std::move(gen);
+        this->quality = std::move(quality);
+        length = std::stoi(head1.substr(head1.find('=') + 1));
+    }
+
+    void trimming(int pos) {
+        gen = gen.substr(pos);
+        quality = quality.substr(0,pos);
+        length = gen.size();
+    }
+};
+
+std::vector<gen_reading> reader(const std::string& file_name) {
+    std::ifstream file(file_name);
+    if (!file.is_open()) {
+        std::cout << "Cannot open file." << std::endl;
+        return {};
+    }
+    std::string head1, head2, gen, quality;
+    std::vector<gen_reading> gens;
+    while (std::getline(file, head1)) {
+        std::getline(file, gen);
+        std::getline(file, head2);
+        std::getline(file, quality);
+        gens.emplace_back(head1, gen, quality);
+    }
+    file.close();
+    return gens;
+}
+
+void print_stats(const std::vector<gen_reading>& gens) {
+    int max = 0;
+    long long cnt = 0;
+    long long sum = 0;
+    int min = 1e7;
+    for (auto & gen : gens) {
+        max = std::max(max, gen.length);
+        cnt++;
+        sum += gen.length;
+        min = std::min(min, gen.length);
+    }
+    std::cout << "total quantity: " << cnt << std::endl;
+    std::cout << "minimum length: " << min << std::endl;
+    std::cout << "maximum length: " << max << std::endl;
+    std::cout << "average length: " << sum / cnt << std::endl;
+}
+
+double gc_stat(const std::vector<gen_reading>& gens) {
+    long long count = 0;
+    long long sum = 0;
+    for (auto & gen : gens) {
+        sum += gen.length;
+        count += std::ranges::count(gen.gen, 'C') +
+            std::ranges::count(gen.gen, 'G');
+    }
+    std::cout << "GC-content: " << count * 100. / sum << std::endl;
+    return count * 100. / sum;
+}
+
+double average_quality_at_position(const std::vector<gen_reading>& gens, int pos) {
+    long long cnt = 0;
+    long long sum = 0;
+    for (auto & gen : gens) {
+        sum += gen.quality[pos] - 33;
+        cnt++;
+    }
+    std::cout << "average quality at position " << pos << ": " << sum / cnt << std::endl;
+    return sum * 1. / cnt;
+}
+
+std::vector<gen_reading> trimming(const std::vector<gen_reading>& gens, int len, int q) {
+    std::vector<gen_reading> res;
+    int over_quality = len * q;
+    for (auto gen : gens) {
+        if (gen.length < len)
+            continue;
+        long long sum = 0;
+        int last = gen.length;
+        for (int i = 0; i < len; i++) {
+            sum += gen.quality[i] - 33;
+        }
+        if (sum < over_quality) {
+            continue;
+        }
+        for (int i = 0; i < gen.length; i++) {
+            sum += gen.quality[i] - gen.quality[i - len];
+            if (sum <= over_quality) {
+                last = i;
+                break;
+            }
+        }
+
+        int i = last;
+
+        int lastBaseQuality = gen.quality[i - 1] - 33;
+        while (lastBaseQuality < q && i > 1) {
+            i--;
+            lastBaseQuality = gen.quality[i - 1] - 33;
+        }
+
+        if (i != 0) {
+            if (i != gen.length) {
+                // std::cout << i << ' ';
+                gen.trimming(i);
+            }
+            res.push_back(gen);
+        }
+    }
+    return res;
+}
+
+int main() {
+    std::vector<gen_reading> gens = reader("reads.fastq");
+    print_stats(gens);
+    gc_stat(gens);
+    average_quality_at_position(gens, 9);
+    auto trim1 = trimming(gens, 5, 30);
+    std::cout << std::endl;
+    std::cout << "cut off readings: " << gens.size() - trim1.size() << std::endl;
+    print_stats(trim1);
+    // auto trim2 = trimming(gens, 60, 30);
+    // std::cout << "size: " << trim2.size() << std::endl;
+    return 0;
+}
